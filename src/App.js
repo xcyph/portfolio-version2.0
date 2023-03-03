6b125fb9f7d901770516bc80cbe93a3e6c35d116
Sarah Teixeira <65655613+xcyph@users.noreply.github.com>
2023-03-03 21:20:05 +0100

    Update App.js
    initial commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Working on this site  
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/xcyph"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Sarah Teixeira
         </a>
       </header>
     </div>
